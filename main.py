a7bdf627175941b173c88441490695003d99a0c1
sunnsnow <00_burpee_ale@icloud.com>
2023-08-13 16:20:48 +0900

    Make selenium able to search and find results

--- main.py
+++ main.py
@@ -2,9 +2,25 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
 
 browser = webdriver.Chrome(service=ChromeService(
-    ChromeDriverManager().install()))
+    ChromeDriverManager().install()), options=chrome_options)
+
+browser.get("https://google.com")
+search_bar = browser.find_element(By.CLASS_NAME, "gLFyf")
+
+search_bar.send_keys("hello", Keys.ENTER)
+
+search_results = browser.find_elements(By.CLASS_NAME, "g")
+
+for search_result in search_results:
+    title = search_result.find_element(By.TAG_NAME, "h3")
+    if title:
+        print(title.text)
+
+browser.quit()
