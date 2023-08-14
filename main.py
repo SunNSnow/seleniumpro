c63c02909a585bcdbe894b550a7ac4e1adff7ac2
sunnsnow <00_burpee_ale@icloud.com>
2023-08-14 22:50:53 +0900

    Make slelnium able to take a screenshot of result

--- main.py
+++ main.py
@@ -4,23 +4,29 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
-chrome_options = Options()
-chrome_options.add_experimental_option("detach", True)
+KEYWORD = "buy domain"
+
 
 browser = webdriver.Chrome(service=ChromeService(
-    ChromeDriverManager().install()), options=chrome_options)
+    ChromeDriverManager().install()))
 
 browser.get("https://google.com")
 search_bar = browser.find_element(By.CLASS_NAME, "gLFyf")
 
-search_bar.send_keys("hello", Keys.ENTER)
+search_bar.send_keys(KEYWORD, Keys.ENTER)
+
+shitty_element = WebDriverWait(browser, 10).until(
+    EC.presence_of_element_located((By.CLASS_NAME, "cUnQKe")))
+
+browser.execute_script("""
+const shit = arguments[0];
+shit.parentElement.removeChild(shit)
+""", shitty_element)
 
 search_results = browser.find_elements(By.CLASS_NAME, "g")
 
-for search_result in search_results:
-    title = search_result.find_element(By.TAG_NAME, "h3")
-    if title:
-        print(title.text)
-
-browser.quit()
+for index, search_result in enumerate(search_results):
+    search_result.screenshot(f"screenshots/{KEYWORD}x{index}.png")
